582b65deb2e235958bbdab984da3b6c511be0fb4
Daggy <Daggy.od@gmail.com>
2022-06-20 22:37:54 +0300

    Практика , ч3. Используем функции

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    // numberOfFilms == '' проверка на пустую строкц || numberOfFilms == null проверка на ноль || isNaN(numberOfFilms) проверка на отмену
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) { 
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+ start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -25,33 +19,59 @@ const personalMovieDB = {
     privat: false
 };
 
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) /* * */ {  
-            personalMovieDB.movies[a] = b;
-            console.log('dobe');
-        } else {
-            console.log('error');
-            i--; /* * */
-        }
-
-
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) /* * */ {  
+                personalMovieDB.movies[a] = b;
+                console.log('dobe');
+            } else {
+                console.log('error');
+                i--; /* * */
+            }
+    }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) { 
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) { 
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+//2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+//false - выводит в консоль главный объект программы
+
+function showMyBd(hidden) { // проверяет на приватность базы. Есть приватная не выводит данные
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyBd(personalMovieDB.privat);
+
+//3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+//"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+//genres
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); // для пользователя выводим с 1 а для базы нужно указать с нуля. (нумерация в массиве с нуля)
+    }
+}
+writeYourGenres();
+
 
 /* a != null && b != null - если нажимаем отмену. 
 a != '' && b != '' - проверка на пустые строки, 
